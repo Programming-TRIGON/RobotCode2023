cb7a9d9ab21371411748f333fc5d97c15f6b9d0d
Yishai Levy <96019039+thesnipe12@users.noreply.github.com>
2023-02-03 04:39:44 +0200

    Added `POSE_ESTIMATOR` to `RobotContainer`

--- src/main/java/frc/trigon/robot/RobotContainer.java
+++ src/main/java/frc/trigon/robot/RobotContainer.java
@@ -1,6 +1,10 @@
 package frc.trigon.robot;
 
+import frc.trigon.robot.subsystems.swerve.PoseEstimator;
+
 public class RobotContainer {
+    public static final PoseEstimator POSE_ESTIMATOR = new PoseEstimator();
+
     public RobotContainer() {
     }
 }
