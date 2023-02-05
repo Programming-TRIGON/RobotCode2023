638ecc8e4626fca6cd4f65ffcc658650b5a65b23
Yishai Levy <96019039+thesnipe12@users.noreply.github.com>
2023-02-05 23:15:32 +0200

    Changed number in `VISION_CALCULATIONS_AMBIGUITY` to be more accurate

--- src/main/java/frc/trigon/robot/subsystems/swerve/PoseEstimatorConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/swerve/PoseEstimatorConstants.java
@@ -3,7 +3,6 @@ package frc.trigon.robot.subsystems.swerve;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.math.util.Units;
 
 public class PoseEstimatorConstants {
     static final int GYRO_UPDATE_DELAY_MS = 15;
@@ -18,5 +17,5 @@ public class PoseEstimatorConstants {
      */
     static final Vector<N3>
             STATES_AMBIGUITY = VecBuilder.fill(0.005, 0.005, 0.0005),
-            VISION_CALCULATIONS_AMBIGUITY = VecBuilder.fill(0.3, 0.3, Units.degreesToRadians(4));
+            VISION_CALCULATIONS_AMBIGUITY = VecBuilder.fill(0.05, 0.05, 0.005);
 }
