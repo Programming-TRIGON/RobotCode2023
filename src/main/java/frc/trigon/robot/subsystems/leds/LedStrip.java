f0ffde2f585a7465e8fc4d8ea1a6bf68c6b8a20b
Mboesn <yishayamir@gmail.com>
2023-02-11 21:07:18 +0200

    added inverted function

--- src/main/java/frc/trigon/robot/subsystems/leds/LedStrip.java
+++ src/main/java/frc/trigon/robot/subsystems/leds/LedStrip.java
@@ -6,11 +6,11 @@ import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.trigon.robot.Robot;
 
-import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
 public class LedStrip extends SubsystemBase {
     private int startingPosition, endingPosition;
+    private final boolean inverted;
     private static final AddressableLED leds = LedsConstants.LED;
     private static final AddressableLEDBuffer LED_BUFFER = new AddressableLEDBuffer(LedsConstants.LEDS_LENGTH);
 
@@ -25,20 +25,23 @@ public class LedStrip extends SubsystemBase {
      * LedStrip represents a subsection of the one long LED strip on the robot.
      *
      * @param startingPosition the first LED in the strip
-     * @param endingPosition the last LED in the strip
+     * @param endingPosition   the last LED in the strip
+     * @param inverted
      */
-    public LedStrip(int startingPosition, int endingPosition) {
+    public LedStrip(int startingPosition, int endingPosition, boolean inverted) {
         this.startingPosition = startingPosition;
         this.endingPosition = endingPosition;
+        this.inverted = inverted;
     }
 
-    public void setLedsColors(Color[] colors){
-        if (colors.length != getLength()){
+    public void setLedsColors(Color[] colors) {
+        if (colors.length != getLength()) {
             Logger.getGlobal().warning("frc.trigon.robot.subsystems.leds.LedStrip.setLedsColors(): Tried to apply a array that not in the correct size");
             return;
         }
-        for (int i = startingPosition; i < endingPosition + 1; i++){
-            LED_BUFFER.setLED(i,convertToGrbIfIsReal(applyBrightness(colors[i - startingPosition], 1)));
+        colors = setInverted(inverted, colors);
+        for (int i = startingPosition; i < endingPosition + 1; i++) {
+            LED_BUFFER.setLED(i, convertToGrbIfIsReal(applyBrightness(colors[i - startingPosition], 1)));
         }
         leds.setData(LED_BUFFER);
     }
@@ -47,17 +50,27 @@ public class LedStrip extends SubsystemBase {
         return endingPosition - startingPosition + 1;
     }
 
-    private static Color applyBrightness(Color color, double brightness){
-        return new Color(color.red * brightness , color.green * brightness , color.blue * brightness);
+    private static Color applyBrightness(Color color, double brightness) {
+        return new Color(color.red * brightness, color.green * brightness, color.blue * brightness);
     }
 
     private static Color rgbToGrb(Color color) {
-            return new Color(color.green, color.red, color.blue);
+        return new Color(color.green, color.red, color.blue);
     }
 
-    private static Color convertToGrbIfIsReal(Color color){
-        if (Robot.isReal()){
+    private static Color convertToGrbIfIsReal(Color color) {
+        if (Robot.isReal()) {
             return rgbToGrb(color);
-        }else return color;
+        } else return color;
+    }
+
+    private static Color[] setInverted(boolean inverted, Color[] colors) {
+        Color[] newColors = new Color[colors.length];
+        if (inverted) {
+            for (int i = 0; i < colors.length; i++) {
+                newColors[i] = colors[colors.length - i - 1];
+            }
+            return newColors;
+        } else return colors;
     }
 }
