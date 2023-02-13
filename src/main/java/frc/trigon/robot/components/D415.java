8e60da2fb3fb45d11f6b57279d923d1a9fba349c
Yishai Levy <96019039+thesnipe12@users.noreply.github.com>
2023-02-13 19:17:52 +0200

    Optimized imports

--- src/main/java/frc/trigon/robot/components/D415.java
+++ src/main/java/frc/trigon/robot/components/D415.java
@@ -1,12 +1,13 @@
 package frc.trigon.robot.components;
 
 import com.google.gson.annotations.SerializedName;
-import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Quaternion;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.trigon.robot.utilities.JsonHandler;
 
 import java.util.ArrayList;
