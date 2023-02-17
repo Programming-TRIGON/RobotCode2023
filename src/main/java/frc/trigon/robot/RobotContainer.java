1c97051d8dc54b69f65b71aaa9a087cd79801dc6
Yishai Levy <96019039+thesnipe12@users.noreply.github.com>
2023-02-17 02:43:06 +0200

    Used `XboxController` in `RobotContainer`

--- src/main/java/frc/trigon/robot/RobotContainer.java
+++ src/main/java/frc/trigon/robot/RobotContainer.java
@@ -1,6 +1,11 @@
 package frc.trigon.robot;
 
+import frc.trigon.robot.componenets.XboxController;
+
 public class RobotContainer {
+    private final double driveControllerDeadband = 0.1;
+    private final XboxController driveController = new XboxController(0, true, driveControllerDeadband);
+
     public RobotContainer() {
     }
 }
