31b695e8216e41387c7db842a171b7c6ae62b366
Yishai Levy <96019039+thesnipe12@users.noreply.github.com>
2023-02-21 16:52:15 +0200

    Renamed `driveController` to `driverController`

--- src/main/java/frc/trigon/robot/RobotContainer.java
+++ src/main/java/frc/trigon/robot/RobotContainer.java
@@ -3,7 +3,7 @@ package frc.trigon.robot;
 import frc.trigon.robot.componenets.XboxController;
 
 public class RobotContainer {
-    private final XboxController driveController = DriverConstants.DRIVE_CONTROLLER;
+    private final XboxController driverController = DriverConstants.DRIVE_CONTROLLER;
 
     public RobotContainer() {
     }
