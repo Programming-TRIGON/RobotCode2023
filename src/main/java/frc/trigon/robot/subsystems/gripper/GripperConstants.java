ecf4f7998198d339b2607ae3cd896e6b467505db
laviRZ <laviarzi@gmail.com>
2023-02-21 18:22:54 +0200

    factory reset motor on startup, add inverted option, actual values for collection

--- src/main/java/frc/trigon/robot/subsystems/gripper/GripperConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/gripper/GripperConstants.java
@@ -7,8 +7,10 @@ public class GripperConstants {
     private static final int MOTOR_ID = 13;
     private static final int POWER_DISTRIBUTION_PORT = 2;
 
+    private static final boolean INVERTED = false;
+
     private static final double
-            HOLD_TRIGGER_DURATION = 0.2,
+            HOLD_TRIGGER_DURATION = 0.1,
             HOLD_TRIGGER_CURRENT = 6;
     static final PowerDistributionManager.CurrentLimitConfig HOLD_TRIGGER_CONFIG = new PowerDistributionManager.CurrentLimitConfig(
             POWER_DISTRIBUTION_PORT,
@@ -18,10 +20,16 @@ public class GripperConstants {
 
     static final WPI_TalonFX MOTOR = new WPI_TalonFX(MOTOR_ID);
 
+    static {
+        MOTOR.configFactoryDefault();
+
+        MOTOR.setInverted(INVERTED);
+    }
+
     enum GripperState {
         STOP(0),
         COLLECT(-0.9),
-        EJECT(0.9),
+        EJECT(0.2),
         HOLD(-0.075);
 
         final double power;
