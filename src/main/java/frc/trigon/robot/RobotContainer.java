951cba8250ec661746266efb6befcfb49d236e67
Yishai Levy <96019039+thesnipe12@users.noreply.github.com>
2023-02-21 16:53:10 +0200

    Custom controller (#5)
    * Added `XboxController`
    
    * Used `XboxController` in `RobotContainer`
    
    * Added a shifting mode
    
    * Removed the shift mode option, and made `square` be `exponent`
    
    * Changed docs and made `calculateValue` have the correct order
    
    * Formatted and changed `driveControllerDeadband` to 0.05
    
    * Added `DriverConstants`, and formatted
    
    * Renamed `driveController` to `driverController`
    
    ---------
    
    Co-authored-by: yisha23 <112813483+yisha23@users.noreply.github.com>

--- src/main/java/frc/trigon/robot/RobotContainer.java
+++ src/main/java/frc/trigon/robot/RobotContainer.java
@@ -1,6 +1,10 @@
 package frc.trigon.robot;
 
+import frc.trigon.robot.componenets.XboxController;
+
 public class RobotContainer {
+    private final XboxController driverController = DriverConstants.DRIVE_CONTROLLER;
+
     public RobotContainer() {
     }
 }
