6c942091d072fb0773eba4d4bd08c454c913dc90
Yishai Levy <96019039+thesnipe12@users.noreply.github.com>
2023-02-23 17:40:14 +0200

    Changed some values

--- src/main/java/frc/trigon/robot/subsystems/swerve/trihard/TrihardSwerveConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/swerve/trihard/TrihardSwerveConstants.java
@@ -33,16 +33,16 @@ public class TrihardSwerveConstants {
     };
     static final SwerveDriveKinematics KINEMATICS = new SwerveDriveKinematics(LOCATIONS);
     static final PIDConstants
-            TRANSLATION_PID_CONSTANTS = new PIDConstants(7, 0, 0),
-            ROTATION_PID_CONSTANTS = new PIDConstants(9, 0, 0);
+            TRANSLATION_PID_CONSTANTS = new PIDConstants(12, 0, 0),
+            ROTATION_PID_CONSTANTS = new PIDConstants(15, 0, 0);
     private static final int PIGEON_ID = 0;
     static final Pigeon2 GYRO = new Pigeon2(PIGEON_ID);
     private static final TrapezoidProfile.Constraints ROTATION_CONSTRAINTS = new TrapezoidProfile.Constraints(
-            1,
-            0.5
+            720,
+            1200
     );
     static final ProfiledPIDController ROTATION_CONTROLLER = new ProfiledPIDController(
-            0.01,
+            12,
             0,
             0,
             ROTATION_CONSTRAINTS
