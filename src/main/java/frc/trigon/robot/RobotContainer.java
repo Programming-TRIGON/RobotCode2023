7519d83e8496df135542ca5058ce4f41afc05e5d
Yishai Levy <96019039+thesnipe12@users.noreply.github.com>
2023-02-27 11:23:03 +0200

    Removed testing

--- src/main/java/frc/trigon/robot/RobotContainer.java
+++ src/main/java/frc/trigon/robot/RobotContainer.java
@@ -1,14 +1,6 @@
 package frc.trigon.robot;
 
-import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.trigon.robot.subsystems.leds.LedStrip;
-import frc.trigon.robot.subsystems.leds.commands.MovingColorsLEDCommand;
-import frc.trigon.robot.subsystems.leds.commands.PingPongLEDCommand;
-
 public class RobotContainer {
     public RobotContainer() {
-        Command command = new MovingColorsLEDCommand(Color.kBlack, Color.kRed, 0.02, 5, new LedStrip(0, 80, false));
-        command.schedule();
     }
 }
